f9eccb69cc720ed0e91928b0793f452ce17682bd
ShafiArif8 <shafiengineer8@gmail.com>
2025-02-20 16:31:57 +0500

    Add files via upload

---
+++ array.js
@@ -0,0 +1,27 @@
+let myarry = ['a','b','vdds','jdcbdsj','ghvfyhivgb','cbsvhivb']
+console.log(myarry);
+//add in end
+let arrup =myarry
+arrup.push('shafi')
+console.log(arrup);
+// to substract from end
+arrup.pop()
+console.log(arrup);
+// to substract from start
+arrup.shift()
+console.log(arrup);
+// add in start
+arrup.unshift('you')
+console.log(arrup);
+//call specific value
+console.log(arrup[0]);
+//to delete m,id elements we uses .splice()
+arrup.splice(1,2)
+console.log(arrup);
+//if we wanna count element of array so definetly it wi;ll start from 1 but js index starts from 0 but need to be tens but there's a method for it .length
+console.log(arrup.length);
+// change specific values
+arrup[0] = 'mee'
+console.log(arrup);
+//
+
