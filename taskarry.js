03e3ec1ba053d45a6e215784f36fda8def791673
ShafiArif8 <shafiengineer8@gmail.com>
2025-02-20 17:08:18 +0500

    Add files via upload

--- taskarry.js
+++ taskarry.js
@@ -14,9 +14,19 @@ myarry.reverse()
 console.log(myarry);
 console.log(myarry.indexOf('banana'));
 let cherin = myarry.indexOf('cherry')
+console.log('way 1');
+
 myarry[cherin]= 'grapes'
+console.log(myarry[-1],'it will go to -1 cuz cherry doesnt exists');
+console.log('way 2');
+
+myarry = ['apple', 'banana', 'cherry' ]
 console.log(myarry);
 
+cherin = myarry.indexOf('cherry')
+myarry[cherin]= 'grapes'
+
+console.log(myarry);
 let numberarr = [1,2,3,4,5,6,7,8,9,10]
 numberarr.splice(4,3)
 console.log(numberarr);
